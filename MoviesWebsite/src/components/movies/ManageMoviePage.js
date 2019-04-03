9449c67d892b658c0b120223b9d1b9718796c925
matthewko-acl <matthew_ko@acl.com>
2019-04-03 07:05:34 -0700

    Readded some code that was missing

---
+++ MoviesWebsite/src/components/movies/ManageMoviePage.js
@@ -0,0 +1,150 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as movieActions from '../../actions/movieActions';
+import MovieForm from './MovieForm';
+import toastr from 'toastr';
+
+export class ManageMoviePage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            movie: Object.assign({}, this.props.movie),
+            errors: {},
+            saving: false
+        };
+        this.updateMovieState = this.updateMovieState.bind(this);
+        this.saveMovie = this.saveMovie.bind(this);
+        this.deleteMovie = this.deleteMovie.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.movie && (this.props.movie.titleId != nextProps.movie.titleId)) {
+            this.setState({movie:Object.assign({}, nextProps.movie)});
+        }
+    }
+
+    deleteMovie(event) {
+        event.preventDefault();
+        this.props.actions.removeMovie(this.state.movie.titleId)
+        .then(() => {
+            this.redirectDeleted();
+        })
+        .catch((error) => {
+            toastr.error('Movie Saving Failed: ' + error);
+        });
+    }
+
+    updateMovieState(event) {
+        const field = event.target.name;
+        let movie = this.state.movie;
+        movie[field] = event.target.value;
+        return this.setState({movie:movie});
+    }
+
+    movieFormIsValid() {
+        let formIsValid = true;
+        let errors = {};
+
+        if (this.state.movie.titleType.length < 3) {
+            errors.titleType = 'Title must be at least 3 characters.';
+            formIsValid = false;
+        }
+
+        this.setState({errors:errors});
+        return formIsValid;
+    }
+
+    saveMovie(event) {
+        event.preventDefault();
+
+        if (!this.movieFormIsValid()) {
+            return;
+        }
+        this.setState({saving:true});
+        this.props.actions.saveMovie(this.state.movie)
+        .then(() => {
+            this.redirect();
+        })
+        .catch((error) => {
+            toastr.error('Movie Saving Failed: ' + error);
+        })
+        .finally(() => {
+            this.setState({saving:false});
+        });
+    }
+
+    redirect() {
+        toastr.success('Movie Saved');
+        this.context.router.push('/movies');
+    }
+
+    redirectDeleted() {
+        toastr.success('Movie Deleted');
+        this.context.router.push('/movies');
+    }
+
+    render() {
+        return (
+            <MovieForm
+                onChange={this.updateMovieState}
+                onSave={this.saveMovie}
+                errors={this.state.errors}
+                movie={this.state.movie}
+                saving={this.state.saving}
+                onDelete={this.deleteMovie}
+            />
+        );
+    }
+}
+
+ManageMoviePage.propTypes = {
+    movie: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+//this have to be always after class
+ManageMoviePage.contextTypes = {
+    router: PropTypes.object
+};
+
+function getMovieById(movies, id) {
+    const movie = movies.filter(movie => movie.titleId === id);
+    return (movie.length) ? movie[0] : null;
+}
+
+//react is updating the form to show up new results
+function mapStateToProps(state, ownProps) {
+
+    const movieId = ownProps.params.id;
+
+    let movie = {
+        titleId: '',
+        titleType:'',
+        primaryTitle:'',
+        originalTitle:'',
+        isAdult:false,
+        startYear: 0,
+        endYear: 0,
+        runtimeMinutes: 0,
+        genres: ''
+    };
+
+    if (movieId && state.movies.length > 0) {
+        movie = getMovieById(state.movies, movieId);
+    }
+
+    return {
+        movie: movie
+    };
+}
+
+//react is matching the creating of the action code to the code
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(movieActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageMoviePage);
---
+++ MoviesWebsite/src/components/movies/MoviesPage.js
@@ -0,0 +1,96 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as movieActions from '../../actions/movieActions';
+import { bindActionCreators } from 'redux';
+import MovieList from './MovieList';
+//import MovieList2 from './MovieList2';
+import {browserHistory} from 'react-router';
+import _ from 'lodash';
+import SearchBar from '../common/SearchBar';
+import toastr from 'toastr';
+
+class MoviesPage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.redirectToAddMoviePage = this.redirectToAddMoviePage.bind(this);
+        this.updateTitleState = this.updateTitleState.bind(this);
+        this.state = {
+            errors: {},
+            searchTitle: ''
+        };
+
+        this.searchMovies = this.searchMovies.bind(this);
+    }
+
+    redirectToAddMoviePage(event) {
+        browserHistory.push('/movie');
+    }
+
+    updateTitleState(event) {
+        const field = event.target.value;
+        return this.setState({searchTitle:field});
+    }
+
+    searchMovies(event) {
+        event.preventDefault();
+        this.props.actions.searchMovies(this.state.searchTitle)
+        .then(() => {
+            toastr.success('Search Completed');
+        })
+        .catch((error) => {
+            toastr.error('Movie Saving Failed: ' + error);
+        });
+    }
+
+    render() {
+        const {movies} = this.props;
+
+        return (
+            <div>
+                <h1>Movies Page</h1>
+                <input
+                    type="submit"
+                    value="Add Movie"
+                    className="btn btn-primary"
+                    onClick={this.redirectToAddMoviePage} />
+                <SearchBar
+                    name="search"
+                    label="Search Movies List"
+                    placeholder="Search"
+                    onChange={this.updateTitleState}
+                    onClick={this.searchMovies}
+                    value={this.searchTitle}
+                />
+                <MovieList movies={movies}/>
+            </div>
+        );
+    }
+}
+
+MoviesPage.propTypes = {
+    movies: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+//this have to be always after class
+MoviesPage.contextTypes = {
+    router: PropTypes.object
+};
+
+
+//react is updating the form to show up new results
+function mapStateToProps(state, ownProps) {
+    return {
+        movies: state.movies
+    };
+}
+
+//react is matching the creating of the action code to the code
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(movieActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesPage);
